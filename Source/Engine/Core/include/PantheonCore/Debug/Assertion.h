0c88b98048dfe83f901b52bfe0bd5334b0fbac58
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2023-10-16 00:20:10 -0400

    Added optional message parameter to assert macro

--- Source/Engine/Core/include/PantheonCore/Debug/Assertion.h
+++ Source/Engine/Core/include/PantheonCore/Debug/Assertion.h
@@ -1,14 +1,25 @@
 #pragma once
+#include <cstdlib>
+
+#include "PantheonCore/Debug/Logger.h"
 
 #ifndef ASSERT
 #ifdef _DEBUG
 
-#define ASSERT(condition) if (!(condition)) __debugbreak()
+#define ASSERT(condition, ...) if (!(condition))    \
+{                                                   \
+    __VA_OPT__(DEBUG_LOG_ERROR(__VA_ARGS__);)       \
+    __debugbreak();                                 \
+    abort();                                        \
+} ((void)0)
 
 #else
 
-#include <cstdlib>
-#define ASSERT(condition) if (!(condition)) abort()
+#define ASSERT(condition, ...) if (!(condition))    \
+{                                                   \
+    __VA_OPT__(DEBUG_LOG_ERROR(__VA_ARGS__);)       \
+    abort();                                        \
+} ((void)0)
 
 #endif // _DEBUG
 #endif // !ASSERT
