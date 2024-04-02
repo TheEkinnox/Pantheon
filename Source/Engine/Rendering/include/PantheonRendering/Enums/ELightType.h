bafca237c66cc0b6bb861965ee85c691d867ad2b
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-02 14:44:48 -0400

    Implemented string <-> light type conversion

--- Source/Engine/Rendering/include/PantheonRendering/Enums/ELightType.h
+++ Source/Engine/Rendering/include/PantheonRendering/Enums/ELightType.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <PantheonCore/Debug/Assertion.h>
+
 #include <cstdint>
 
 namespace PantheonRendering::Enums
@@ -13,4 +15,52 @@ namespace PantheonRendering::Enums
         POINT,
         SPOT
     };
+
+    /**
+     * \brief Gets the given light type's string representation
+     * \param type The light type to stringify
+     * \return The given type's string representation
+     */
+    inline std::string lightTypeToString(const ELightType type)
+    {
+        switch (type)
+        {
+        case ELightType::AMBIENT:
+            return "ambient";
+        case ELightType::DIRECTIONAL:
+            return "directional";
+        case ELightType::POINT:
+            return "point";
+        case ELightType::SPOT:
+            return "spot";
+        default:
+            ASSERT(false, "Unsupported light type");
+            return {};
+        }
+    }
+
+    /**
+     * \brief Gets the light type represented by the given string
+     * \param type The type string to convert
+     * \return The type corresponding to the given string
+     */
+    inline ELightType stringToLightType(std::string type)
+    {
+        PantheonCore::Utility::toLowerInPlace(type);
+
+        if (type == "ambient")
+            return ELightType::AMBIENT;
+
+        if (type == "directional")
+            return ELightType::DIRECTIONAL;
+
+        if (type == "point")
+            return ELightType::POINT;
+
+        if (type == "spot")
+            return ELightType::SPOT;
+
+        ASSERT(false, "Unsupported light type");
+        return ELightType::AMBIENT;
+    }
 }
