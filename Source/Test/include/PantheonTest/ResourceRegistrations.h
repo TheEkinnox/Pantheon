6df58b7e978c4582eb678827a93a33c7fbb33da6
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-05 17:19:12 -0400

    Added rendering resources to test resource registrations

--- Source/Test/include/PantheonTest/ResourceRegistrations.h
+++ Source/Test/include/PantheonTest/ResourceRegistrations.h
@@ -1,4 +1,14 @@
 #pragma once
 #include <PantheonCore/ECS/Scene.h>
 
+#include <PantheonRendering/Resources/Material.h>
+#include <PantheonRendering/Resources/Model.h>
+#include <PantheonRendering/RHI/IShader.h>
+#include <PantheonRendering/RHI/ITexture.h>
+
 REGISTER_RESOURCE_TYPE(Scene, PantheonCore::ECS::Scene)
+
+REGISTER_RESOURCE_TYPE(Material, PantheonRendering::Resources::Material)
+REGISTER_RESOURCE_TYPE(Model, PantheonRendering::Resources::Model)
+REGISTER_RESOURCE_TYPE(Shader, PantheonRendering::RHI::IShader)
+REGISTER_RESOURCE_TYPE(Texture, PantheonRendering::RHI::ITexture)
