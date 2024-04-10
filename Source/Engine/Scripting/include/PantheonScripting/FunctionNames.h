e34abb5a5db655e7e034044215afd32697496751
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-09 23:21:09 -0400

    Reordered function names struct to match execution order

--- Source/Engine/Scripting/include/PantheonScripting/FunctionNames.h
+++ Source/Engine/Scripting/include/PantheonScripting/FunctionNames.h
@@ -8,7 +8,7 @@ namespace PantheonScripting
         static constexpr const char* START        = "onStart";
         static constexpr const char* UPDATE       = "onUpdate";
         static constexpr const char* FIXED_UPDATE = "onFixedUpdate";
-        static constexpr const char* DESTROY      = "onDestroy";
         static constexpr const char* STOP         = "onStop";
+        static constexpr const char* DESTROY      = "onDestroy";
     };
 }
