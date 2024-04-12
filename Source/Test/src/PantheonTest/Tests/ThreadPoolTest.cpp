da6459bfc307daf170de07077d9ad425fdc965ca
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-12 02:42:32 -0400

    Reworked thread pool test

--- Source/Test/src/PantheonTest/Tests/ThreadPoolTest.cpp
+++ Source/Test/src/PantheonTest/Tests/ThreadPoolTest.cpp
@@ -19,33 +19,25 @@ namespace PantheonTest
     void ThreadPoolTest::onStart()
     {
         DEBUG_LOG("Testing thread pool - Executing %llu tasks of %llums", m_taskCount, m_taskDuration);
+        const auto start = std::chrono::high_resolution_clock::now();
 
-        auto start = std::chrono::high_resolution_clock::now();
-
-        for (size_t i = 0; i < m_taskCount; ++i)
-            std::this_thread::sleep_for(std::chrono::milliseconds(m_taskDuration));
-
-        auto end = std::chrono::high_resolution_clock::now();
-        DEBUG_LOG("Singe thread: %dms", std::chrono::duration_cast<std::chrono::milliseconds>(end - start));
-
-        start = std::chrono::high_resolution_clock::now();
         std::vector<std::future<void>> tasks;
         tasks.reserve(m_taskCount);
 
-        for (size_t i = 0; i < m_taskCount; ++i)
+        const auto testTask = [taskDuration = m_taskDuration]
         {
-            tasks.emplace_back(m_threadPool->enqueue([taskDuration = m_taskDuration]
-            {
-                std::this_thread::sleep_for(std::chrono::milliseconds(taskDuration));
-            }));
-        }
+            std::this_thread::sleep_for(std::chrono::milliseconds(taskDuration));
+        };
+
+        for (size_t i = 0; i < m_taskCount; ++i)
+            tasks.emplace_back(m_threadPool->enqueue(testTask));
 
         TEST_CHECK(tasks.size() == m_taskCount, "ThreadPool tasks count should be %llu - Received %llu", m_taskCount, tasks.size());
 
         for (auto& task : tasks)
             task.wait();
 
-        end = std::chrono::high_resolution_clock::now();
+        const auto end = std::chrono::high_resolution_clock::now();
         DEBUG_LOG("Multi thread: %dms", std::chrono::duration_cast<std::chrono::milliseconds>(end - start));
 
         complete();
