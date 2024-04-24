4cbafa52d07ee6bb0ec7efb8720a3ed9c0518ec3
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-24 03:31:01 -0400

    Added script template

---
+++ Resources/scripts/template.lua
@@ -0,0 +1,27 @@
+local %%script = {}
+
+-- onInit is called when the script is added
+function %%script:onInit()
+end
+
+-- onStart is called when the application starts
+function %%script:onStart()
+end
+
+-- onFixedUpdate is called every fixed time step
+function %%script:onFixedUpdate(fixedDeltaTime)
+end
+
+-- onUpdate is called once per frame
+function %%script:onUpdate(deltaTime)
+end
+
+-- onStop is called when the application stops
+function %%script:onStop()
+end
+
+-- onDestroy is called when the script is removed
+function %%script:onDestroy()
+end
+
+return %%script
