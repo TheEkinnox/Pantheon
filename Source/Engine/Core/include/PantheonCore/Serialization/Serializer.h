1f9dc75740d1e878e55d7afea473d5d35baf2cfe
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-04-29 14:32:05 -0400

    Added json file loading function

--- Source/Engine/Core/include/PantheonCore/Serialization/Serializer.h
+++ Source/Engine/Core/include/PantheonCore/Serialization/Serializer.h
@@ -1,13 +1,25 @@
 #pragma once
+#include <string>
 #include <vector>
 
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
+#include <rapidjson/ostreamwrapper.h>
 
 namespace PantheonCore::Serialization
 {
     using JsonWriter = rapidjson::Writer<rapidjson::StringBuffer>;
+    using JsonOStream = rapidjson::OStreamWrapper;
+    using JsonFileWriter = rapidjson::Writer<JsonOStream>;
+    using JsonDocument = rapidjson::Document;
     using JsonValue = rapidjson::Value;
+
+    /**
+     * \brief Loads the json document at the given path
+     * \param p_path The json file's path
+     * \return The loaded json document
+     */
+    JsonDocument loadJsonFile(const std::string& p_path);
 
     template <typename T>
     bool toBinary(const T& value, std::vector<char>& out) requires (!std::is_enum_v<T>);
---
+++ Source/Engine/Core/src/PantheonCore/Serialization/Serializer.cpp
@@ -0,0 +1,29 @@
+#include "PantheonCore/Serialization/Serializer.h"
+
+#include <rapidjson/istreamwrapper.h>
+
+#if defined(_DEBUG) || defined(PTH_VERBOSE_LOG)
+#include <rapidjson/error/en.h>
+#endif
+
+namespace PantheonCore::Serialization
+{
+    JsonDocument loadJsonFile(const std::string& path)
+    {
+        std::ifstream fs(path);
+
+        if (!CHECK(fs.is_open(), "Unable to open json file at path \"%s\"", path.c_str()))
+            return {};
+
+        rapidjson::IStreamWrapper isw(fs);
+
+        rapidjson::Document json;
+        json.ParseStream(isw);
+
+        if (!CHECK(!json.HasParseError(), "Unable to parse json from file - Parse error %d (%s)",
+                json.GetParseError(), rapidjson::GetParseError_En(json.GetParseError())))
+            return {};
+
+        return json;
+    }
+}
