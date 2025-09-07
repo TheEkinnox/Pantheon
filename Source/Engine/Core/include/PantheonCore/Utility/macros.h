0a0b2473da85d8a23ace6c24f801e32a24d140a9
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2025-09-07 12:47:09 -0400

    Add CAT, STRINGIFY and UNIQUE_VAR macros

--- Source/Engine/Core/include/PantheonCore/Utility/macros.h
+++ Source/Engine/Core/include/PantheonCore/Utility/macros.h
@@ -7,3 +7,17 @@
 #ifndef ARGS
 #define ARGS(...) __VA_ARGS__
 #endif //!ARGS
+
+#ifndef CAT
+#undef CAT_
+#define CAT_(a, b) a##b
+#define CAT(a, b) CAT_(a, b)
+#endif
+
+#ifndef STRINGIFY
+#undef STRINGIFY_
+#define STRINGIFY_(x) #x
+#define STRINGIFY(x) STRINGIFY_(x)
+#endif
+
+#define UNIQUE_VAR(x) CAT(_, CAT(x, __LINE__))
