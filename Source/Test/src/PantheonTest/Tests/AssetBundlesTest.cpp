d475aef31911fed9e82f169f42b79c407dbaa299
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2025-09-27 13:19:10 -0400

    Reduce allocs in asset bundle tests

--- Source/Test/src/PantheonTest/Tests/AssetBundlesTest.cpp
+++ Source/Test/src/PantheonTest/Tests/AssetBundlesTest.cpp
@@ -37,18 +37,19 @@ namespace PantheonTest
     void AssetBundlesTest::testAdd()
     {
         {
-            std::vector<ResourceAsset> resourceAssets;
-
-            resourceAssets.emplace_back("Texture", "c53a69fd-745c-4cfc-a5db-282947924bc2", "textures/container.jpg");
-            resourceAssets.emplace_back("Material", "9ae1f054-4bfa-4aac-adb9-83af83278c0d", "materials/unlit.pthmat");
-            resourceAssets.emplace_back("Material", "a1e05107-201f-4bda-b31b-e190f6e8e8e5", "materials/lit.pthmat");
-            resourceAssets.emplace_back("Shader", "868b6811-6ab6-4d8a-becf-fabfd3f5d87c", "shaders/Lit.glsl");
-            resourceAssets.emplace_back("Shader", "e8bfefe7-d3f6-43cb-a70d-ffd82c501c71", "shaders/Unlit.glsl");
-            resourceAssets.emplace_back("Model", "0febc1d1-4569-4269-acdd-743557c4ecd6", "meshes/primitives/cube.obj");
-            resourceAssets.emplace_back("Model", "59e50a16-ec45-4d7a-b068-89b64fb12535", "meshes/primitives/sphere.obj");
-            resourceAssets.emplace_back("Scene", "8a5491ad-e3cf-4f32-a748-539170994753", "scenes/empty.pthscene");
-            resourceAssets.emplace_back("Scene", "b6b3bba8-d4fe-4f14-9fac-a10a3bc87c0d", "scenes/pretty.pthscene");
-            resourceAssets.emplace_back("Scene", "65e9a7e1-4986-4147-bf61-9b314e2e44db", "scenes/minified.pthscene");
+            const ResourceAsset resourceAssets[] =
+            {
+                { "Texture", "c53a69fd-745c-4cfc-a5db-282947924bc2", "textures/container.jpg" },
+                { "Material", "9ae1f054-4bfa-4aac-adb9-83af83278c0d", "materials/unlit.pthmat" },
+                { "Material", "a1e05107-201f-4bda-b31b-e190f6e8e8e5", "materials/lit.pthmat" },
+                { "Shader", "868b6811-6ab6-4d8a-becf-fabfd3f5d87c", "shaders/Lit.glsl" },
+                { "Shader", "e8bfefe7-d3f6-43cb-a70d-ffd82c501c71", "shaders/Unlit.glsl" },
+                { "Model", "0febc1d1-4569-4269-acdd-743557c4ecd6", "meshes/primitives/cube.obj" },
+                { "Model", "59e50a16-ec45-4d7a-b068-89b64fb12535", "meshes/primitives/sphere.obj" },
+                { "Scene", "8a5491ad-e3cf-4f32-a748-539170994753", "scenes/empty.pthscene" },
+                { "Scene", "b6b3bba8-d4fe-4f14-9fac-a10a3bc87c0d", "scenes/pretty.pthscene" },
+                { "Scene", "65e9a7e1-4986-4147-bf61-9b314e2e44db", "scenes/minified.pthscene" }
+            };
 
             for (const auto& asset : resourceAssets)
             {
@@ -63,10 +64,11 @@ namespace PantheonTest
         }
 
         {
-            std::vector<Asset> assets;
-
-            assets.emplace_back("Text", "30507b5d-74b5-4514-a517-736d2197a6cc", "assets/shaders/Basic.glsl");
-            assets.emplace_back("Binary", "c94e335e-5be6-4699-b9cc-4e537c75a812", "assets/textures/container2.png");
+            Asset assets[] =
+            {
+                { "Text", "30507b5d-74b5-4514-a517-736d2197a6cc", "assets/shaders/Basic.glsl" },
+                { "Binary", "c94e335e-5be6-4699-b9cc-4e537c75a812", "assets/textures/container2.png" }
+            };
 
             for (const auto& asset : assets)
             {
@@ -83,42 +85,42 @@ namespace PantheonTest
 
     void AssetBundlesTest::testSerialization()
     {
-        std::vector<std::pair<ECompressionMode, std::string>> compressionModes;
-
-        compressionModes.emplace_back(ECompressionMode::NONE, "none");
-        compressionModes.emplace_back(ECompressionMode::ZSTD, "zstd");
-        compressionModes.emplace_back(ECompressionMode::BROTLI, "brotli");
-        compressionModes.emplace_back(ECompressionMode::LZ4, "lz4");
+        static constexpr std::tuple<ECompressionMode, const char*, const char*> compressionModes[] =
+        {
+            { ECompressionMode::NONE, "none", "none.pthbundle" },
+            { ECompressionMode::ZSTD, "zstd", "zstd.pthbundle" },
+            { ECompressionMode::BROTLI, "brotli", "brotli.pthbundle" },
+            { ECompressionMode::LZ4, "lz4", "lz4.pthbundle" }
+        };
 
         double uncompressedSize = 0;
 
-        for (const auto& [compressionMode, modeName] : compressionModes)
+        for (const auto& [compressionMode, modeName, bundlePath] : compressionModes)
         {
-            auto       start  = std::chrono::system_clock::now();
+            const auto start  = std::chrono::system_clock::now();
             const auto assets = m_assetBundle.getAssets();
 
-            std::string bundlePath = modeName + ".pthbundle";
-            PTH_LOG("\nCompression mode: %s", modeName.c_str());
+            PTH_LOG("\nCompression mode: %s", modeName);
 
-            TEST_CHECK(m_assetBundle.save(bundlePath.c_str(), compressionMode),
-                "Unbale to save asset bundle at path \"%s\"", bundlePath.c_str());
+            TEST_CHECK(m_assetBundle.save(bundlePath, compressionMode),
+                "Unbale to save asset bundle at path \"%s\"", bundlePath);
 
             const uint64_t bundleSize = std::filesystem::file_size(bundlePath);
 
             if (compressionMode == ECompressionMode::NONE)
                 uncompressedSize = static_cast<double>(bundleSize);
 
-            auto         writeEnd  = std::chrono::system_clock::now();
+            const auto   writeEnd  = std::chrono::system_clock::now();
             const double writeTime = std::chrono::duration_cast<std::chrono::duration<double>>(writeEnd - start).count();
 
             TEST_CHECK(m_assetBundle.load(bundlePath),
-                "Unbale to load asset bundle at path \"%s\"", bundlePath.c_str());
+                "Unbale to load asset bundle at path \"%s\"", bundlePath);
 
             PTH_LOG("TEST PATHS");
             for (const auto& asset : assets)
                 testAssetAtPath(m_assetBundle, asset);
 
-            auto         readEnd  = std::chrono::system_clock::now();
+            const auto   readEnd  = std::chrono::system_clock::now();
             const double readTime = std::chrono::duration_cast<std::chrono::duration<double>>(readEnd - writeEnd).count();
 
             PTH_LOG("TEST GUIDS");
@@ -127,7 +129,7 @@ namespace PantheonTest
 
             std::filesystem::remove(bundlePath);
 
-            auto         end          = std::chrono::system_clock::now();
+            const auto   end          = std::chrono::system_clock::now();
             const double testDuration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
 
             PTH_LOG("\n%s\t- Test completed in %fs\n"
@@ -135,7 +137,7 @@ namespace PantheonTest
                 "\t- Read: %fs | Read avg.: %fs\n"
                 "\t- Bundle Size: %s | Compression Ratio: %f\n"
                 "\t- Avg. Write Speed: %s/s | Avg. Read Speed: %s/s",
-                modeName.c_str(), testDuration,
+                modeName, testDuration,
                 writeTime, writeTime / static_cast<double>(assets.size()),
                 readTime, readTime / static_cast<double>(assets.size()),
                 sizeToStr(static_cast<double>(bundleSize)).c_str(), uncompressedSize / static_cast<double>(bundleSize),
